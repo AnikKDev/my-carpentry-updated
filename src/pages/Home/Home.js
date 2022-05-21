0c97646363ab4b45a29d07d3c45cd648a3fc3df7
AnikKDev <anikdev2016@gmail.com>
2022-05-22 01:05:52 +0600

    completed sub banner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import SubBanner from './SubBanner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SubBanner></SubBanner>
         </div>
     );
 };
---
+++ src/pages/Home/SubBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SubBanner = () => {
+    return (
+        <div className='flex items-center justify-center'>
+            <div className="text-center w-2/5">
+                <h4 className="text-lg font-semibold">Welcome to our website</h4>
+                <h2 className="text-5xl my-3 font-bold">MyCarpenry</h2>
+                <p className="">One of the leading carpentry tools providing inc. around the globe </p>
+                <hr className="my-8" />
+            </div>
+        </div>
+    );
+};
+
+export default SubBanner;
