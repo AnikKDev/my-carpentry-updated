bda07a092cc163f042c9d958df824bb44cc771c5
AnikKDev <anikdev2016@gmail.com>
2022-05-23 13:03:57 +0600

    complete purchase form

--- src/pages/ToolDetail/ToolDetail.js
+++ src/pages/ToolDetail/ToolDetail.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import axios from "axios";
+import auth from '../../firebase.init';
 const ToolDetail = () => {
+    const [user, loading, error] = useAuthState(auth);
 
     const { id } = useParams();
     const [toolDetail, setToolDetail] = useState({});
@@ -16,7 +19,20 @@ const ToolDetail = () => {
                 console.error(error);
             }
         })();
-    }, [id])
+    }, [id]);
+
+    // form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const purchaseDetail = {
+            name: user.displayName,
+            email: user.email,
+            address: e.target.address.value,
+            phone: e.target.phone.value
+        }
+        console.log(purchaseDetail)
+        // reset()
+    };
     return (
         <div className="lg:h-screen flex items-center justify-center">
             <div class="card lg:card-side lg:mx-24 bg-base-100 shadow-xl">
@@ -27,9 +43,57 @@ const ToolDetail = () => {
                     <p>Price: {price}</p>
                     <p>Minimum Quantity: {minQuantity}</p>
                     <p>Availble Quantity: {availableQuantity}</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Purchase</button>
-                    </div>
+
+                </div>
+                {/* order form */}
+                <div>
+                    <form onSubmit={handleSubmit} class="card-body w-full lg:w-96">
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Name</span>
+                            </label>
+                            <input
+
+                                type="text" value={user.displayName} disabled class="input input-bordered"
+                            />
+
+                        </div>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email" value={user.email} disabled class="input input-bordered"
+                            />
+                        </div>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Address</span>
+                            </label>
+                            <input
+                                type="text" name='address' required placeholder='address' class="input input-bordered"
+                            />
+
+                        </div>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Phone</span>
+                            </label>
+                            <input
+                                type="number" name='phone' required placeholder='phone' class="input input-bordered"
+                            />
+
+                        </div>
+
+
+                        <div class="form-control mt-6">
+                            <button type="submit" class="btn btn-primary">Continue Purchasing</button>
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
