ee69f88b632dc88a6567145053813b62b694808a
AnikKDev <anikdev2016@gmail.com>
2022-05-25 14:13:03 +0600

    completed my profiles functionality

--- src/pages/Dashboard/MyProfile.js
+++ src/pages/Dashboard/MyProfile.js
@@ -1,9 +1,131 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useQuery } from 'react-query';
+import LoadingSpinner from '../../SharedPages/LoadingSpinner';
+import swal from 'sweetalert';
 const MyProfile = () => {
+    const [user] = useAuthState(auth)
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    const [userInfo, setUserInfo] = useState({})
+
+    // get user with specific id
+    const { data: updatedUser, isLoading, refetch } = useQuery('user', () => fetch(`http://localhost:5000/user/${user.email}`, {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    useEffect(() => {
+        if (updatedUser) {
+            setUserInfo(updatedUser);
+        }
+    }, [updatedUser])
+
+
+    if (isLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
+    const onSubmit = ({ phone, name, location, education, linkedin }) => {
+
+
+
+        const userData = {
+            phone: phone,
+            name: name,
+            location: location,
+            education: education,
+            linkedin: linkedin
+        };
+        fetch(`http://localhost:5000/myprofile/${user.email}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    swal("Success", "Updated successfully!", "success");
+                    refetch();
+                    reset();
+
+                } else {
+                    swal("Failed", "Failed to update data. Try again!", "error");
+                }
+            })
+    };
     return (
-        <div>
-            profile
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row">
+                <div class=" lg:text-left">
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <div class="card-body">
+                            <h2 class="card-title">My Profile</h2>
+                            <p>Email: {user.email}</p>
+                            <p>Name: {user.displayName || userInfo.name}</p>
+                            <p>Location: {userInfo.location}</p>
+                            <p>Education: {userInfo.education}</p>
+                            <p>LinkedIn URL: {userInfo.linkedin}</p>
+                            <p>Phone: {userInfo.phone}</p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div class="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="email" name="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " disabled readonly value={user?.email || ''} />
+                                <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                            </div>
+
+
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+                                    {...register("name", { required: true })}
+                                />
+
+                                <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+                                {errors.name?.type === 'required' && <span className="text-error">Name is required</span>}
+                            </div>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required
+                                    {...register("location", { required: true })}
+                                />
+                                <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Location</label>
+                                {errors.location?.type === 'required' && <span className="text-error">Location is required</span>}
+                            </div>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required
+                                    {...register("education", { required: true })}
+                                />
+                                <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Education</label>
+                                {errors.education?.type === 'required' && <span className="text-error">Education field is required</span>}
+                            </div>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required
+                                    {...register("linkedin", { required: true })}
+                                />
+                                <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">LinkedIn</label>
+                                {errors.linkedin?.type === 'required' && <span className="text-error">LikedIn profile  required</span>}
+                            </div>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="tel" pattern="[0-9]{11}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required
+                                    {...register("phone", { required: true })}
+                                />
+                                <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (1234567890)</label>
+                                {errors.phone?.type === 'required' && <span className="text-error">Phone number is required</span>}
+                            </div>
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
