a294b9cfee633ad83e11bf889ad91221edf54cf7
AnikKDev <anikdev2016@gmail.com>
2022-05-31 14:20:39 +0600

    added multi carousel for customer reviews

--- src/pages/Home/Review.js
+++ src/pages/Home/Review.js
@@ -4,8 +4,8 @@ import Rating from 'react-rating';
 const Review = ({ review }) => {
     const { name, comment, rating } = review;
     return (
-        <div className="card w-96 bg-gray-200 shadow-xl">
-            <div className="card-body">
+        <div className="card w-96 h-48 bg-gray-200 shadow-xl">
+            <div className="card-body ">
                 <h2 className="card-title text-primary uppercase">{name}</h2>
                 <p className="text-gray-500"><span className="font-semibold">Comment:</span> {comment}</p>
                 <p>
--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Review from './Review';
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(() => {
@@ -8,17 +9,51 @@ const Reviews = () => {
             .then(res => res.json())
             .then(data => setReviews(data))
     }, []);
+
+    // multi carousel setup
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 768 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 768, min: 0 },
+            items: 1
+        }
+    };
     return (
         <div
             data-aos="fade-up"
             data-aos-duration="1000"
             className="my-16 px-4">
             <h1 className="text-center text-primary font-bold text-4xl uppercase font-['Bree_Serif']">What Our Customers Say</h1>
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-y-5 my-11">
+            {/* <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-y-5 my-11">
+
+            </div> */}
+            <Carousel
+                swipeable={true}
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={3000}
+                keyBoardControl={true}
+                customTransition="transform 300ms ease-in-out"
+                transitionDuration={500}
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+
+                className="py-14" responsive={responsive}>
                 {
                     reviews?.map(review => <Review key={review._id} review={review}></Review>)
                 }
-            </div>
+            </Carousel>
         </div>
     );
 };
